dd90480a310ea60ef0eacf366043436921728754
Artur Y <artyorsh@users.noreply.github.com>
2020-02-10 16:49:41 +0300

    chore: prepare for publishing

--- README.md
+++ README.md
@@ -1,17 +1,13 @@
 # Demo App running Eva Design - Material and UI Kitten
 
-[<img src="https://i.imgur.com/X8O4GHb.png">](https://github.com/akveo/react-native-ui-kitten)
+[<img src="https://i.imgur.com/X8O4GHb.png">][link:ui-kitten]
 
-## How to run
+## Usage
 
-Currently, this project is runnable only in development mode.
-Before running, please follow [dev config](./env/env.dev.js) instructions to make it runnable :]
-
-After environment is configured:
-
+- `git clone https://github.com/artyorsh/ui-kitten-material` to clone the repo
 - `yarn` to install dependencies
 - `cd ios && pod install && cd ..` to install iOS dependencies
-- `yarn start:dev` to start Metro Bundler
+- `yarn start` to start Metro Bundler
 - `npx react-native run-ios` to run on iPhone simulator
 
-Easy!
+[link:ui-kitten]: https://github.com/akveo/react-native-ui-kitten
--- babel.config.js
+++ babel.config.js
@@ -1,27 +1,3 @@
-const path = require('path');
-const env = require('./env');
-
-const frameworkAlias = {
-  '@eva-design/dss': path.resolve(env.EVA_PACKAGES_PATH, 'dss'),
-  '@eva-design/material': path.resolve(env.EVA_PACKAGES_PATH, 'material'),
-  '@eva-design/processor': path.resolve(env.EVA_PACKAGES_PATH, 'processor'),
-  '@ui-kitten/components': path.resolve(env.UI_KITTEN_PACKAGES_PATH, 'components'),
-  '@ui-kitten/eva-icons': path.resolve(env.UI_KITTEN_PACKAGES_PATH, 'eva-icons'),
-};
-
-const frameworkInternalAlias = {
-  '@kitten/theme': path.resolve(env.UI_KITTEN_PACKAGES_PATH, 'components/theme'),
-  '@kitten/ui': path.resolve(env.UI_KITTEN_PACKAGES_PATH, 'components/ui'),
-};
-
-const moduleResolverConfig = {
-  root: path.resolve('./'),
-  alias: {
-    ...frameworkAlias,
-    ...frameworkInternalAlias,
-  },
-};
-
 module.exports = function (api) {
   api.cache(true);
 
@@ -29,9 +5,5 @@ module.exports = function (api) {
     'module:metro-react-native-babel-preset',
   ];
 
-  const plugins = [
-    ['module-resolver', moduleResolverConfig],
-  ];
-
-  return { presets, plugins };
+  return { presets };
 };
